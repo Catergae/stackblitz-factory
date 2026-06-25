10140726ff8d983a42b9f4c928e4228c10bda0a1
Catergae <123557396+Catergae@users.noreply.github.com>
2026-06-25 11:16:25 +0000

    disattiva temporaneamente middleware

--- middleware.ts
+++ middleware.ts
@@ -5,25 +5,5 @@ export const config = {
 };
 
 export default function middleware(request: Request) {
-  const basicAuth = request.headers.get("authorization");
-  const USER = "demo";
-  const PASS = process.env.SITE_PASSWORD;
-
-  if (basicAuth) {
-    const authValue = basicAuth.split(" ")[1] ?? "";
-    const [user, pwd] = atob(authValue).split(":");
-    if (user === USER && pwd === PASS) {
-      return new Response("LOGIN OK! Auth riuscita.", {
-        status: 200,
-        headers: { "content-type": "text/plain" },
-      });
-    }
-  }
-
-  return new Response("Autenticazione richiesta", {
-    status: 401,
-    headers: {
-      "WWW-Authenticate": 'Basic realm="Area riservata"',
-    },
-  });
+  return next();
 }
