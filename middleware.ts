aa37f536dc82b55b598bf97136f2c852707ce3e9
Catergae <123557396+Catergae@users.noreply.github.com>
2026-06-25 10:57:37 +0000

    aggiungi protezione password

---
+++ middleware.ts
@@ -0,0 +1,24 @@
+export const config = {
+  matcher: "/(.*)",
+};
+
+export default function middleware(request: Request) {
+  const basicAuth = request.headers.get("authorization");
+  const USER = "demo";
+  const PASS = process.env.SITE_PASSWORD;
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(" ")[1] ?? "";
+    const [user, pwd] = atob(authValue).split(":");
+    if (user === USER && pwd === PASS) {
+      return; // accesso consentito → prosegue normalmente
+    }
+  }
+
+  return new Response("Autenticazione richiesta", {
+    status: 401,
+    headers: {
+      "WWW-Authenticate": 'Basic realm="Area riservata"',
+    },
+  });
+}
